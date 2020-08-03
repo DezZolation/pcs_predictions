aca0fc178dfe3b69f359f7eb44cdbf1dc4ab7ea0
Mathijs <mathijssonnemans@hotmail.com>
2020-08-03 23:38:14 +0200

    progress

--- pc_db.py
+++ pc_db.py
@@ -1,8 +1,10 @@
 import sqlite3
 import time
 import datetime
+import statistics
 
 from dateutil import parser
+from enum import Enum
 
 db_file = "pcs.db"
 conn = None
@@ -12,9 +14,69 @@ except sqlite3.Error as e:
     print(e)
 cur = conn.cursor()
 
+race_statistics_cache = {}
+
 def query(q):
     cur.execute(q)
     return cur.fetchall()
+
+def get_races_of_type(stage_type):
+    res = query("""
+            SELECT slug FROM races
+            WHERE stage_type = '%s'
+        """ % stage_type)
+    slugs = [row[0] for row in res]
+    return slugs
+
+def get_statistics_for_race(slug):
+    if slug in race_statistics_cache:
+        return race_statistics_cache[slug]
+
+    res = query("""
+        SELECT (distance / stage_time * 3600) AS speed FROM results INNER JOIN races ON results.race = races.slug
+        WHERE race = '%s'
+    """ % slug)
+    times = [row[0] for row in res if isinstance(row[0], float)]
+    stage_mean = statistics.mean(times)
+    std = statistics.pstdev(times)
+    race_statistics_cache[slug] = (stage_mean, std)
+    return race_statistics_cache[slug]
+
+def get_date_of_race(slug):
+    res = query("""
+        SELECT date FROM races WHERE slug = '%s'
+    """ % slug)
+    return res[0][0]
+
+def get_riders_of_race(slug):
+    res = query("""
+        SELECT rider FROM results WHERE race = '%s' AND stage_rank not null ORDER BY stage_rank
+    """ % slug)
+    return [row[0] for row in res]
+
+
+# Time trials with differences from winner in seconds
+# SELECT race, (distance/stage_time*3600) AS average_speed, stage_time, stage_rank, difficulty, distance, profile,
+# stage_time - (SELECT stage_time FROM results WHERE race = slug AND stage_rank = 1) as time_difference_from_winner
+# FROM races INNER JOIN results ON races.slug = results.race WHERE rider = "tom-dumoulin" AND stage_type = "ITT" ORDER BY stage_rank
+
+
+# !!!
+#     SELECT race, stage_time, 
+# (SELECT COUNT(rider) FROM results AS r2 WHERE r1.race = r2.race AND r1.stage_time = r2.stage_time ) 
+# FROM results AS r1 WHERE stage_rank = 1 LIMIT 100  
+
+# class StageType(Enum):
+#     ITT = "stage_type = 'ITT'"
+#     TTT = "stage_type = 'TTT'"
+#     SPRINT = "stage_type = 'REGULAR' AND "
+
+def rider_race_query(rider, stage_type, rank_cutoff = 999, date_cutoff = '2030-12-31'):
+    res = query("""
+            SELECT race, (distance / stage_time * 3600) AS speed, date FROM results INNER JOIN races ON results.race = races.slug 
+            WHERE rider = '%s' AND stage_type = '%s' AND stage_rank <= %d AND date < '%s' ORDER BY date
+        """ % (rider, stage_type, rank_cutoff, date_cutoff))
+    return res
 
 def date_result_query(rider, stage_type, rank_cutoff = 999, season_cutoff = 2000):
     res = query("""
@@ -24,6 +86,20 @@ def date_result_query(rider, stage_type, rank_cutoff = 999, season_cutoff = 2000
     dates = [parser.parse(row[0]) for row in res]
     ranks = [row[1] for row in res]
     return (dates, ranks)
+
+def get_sprints():
+    res = query("""
+            SELECT * FROM results INNER JOIN races ON results.race = races.slug
+            WHERE
+        """)
+    return res
+
+# def effort_ratio_query(rider, stage_type, rank_cutoff = 20, season_cutoff = 2000):
+#     res = query("""
+#             SELECT COUNT(*) FROM results INNER JOIN races ON results.race = races.slug 
+#             WHERE rider = '%s' AND stage_type = '%s' AND stage_rank <= %d AND season >= %d ORDER BY date
+#         """ % (rider, stage_type, rank_cutoff, season_cutoff))
+#     return
 
 # def comparative_result_query(riders, stage_type, season_cutoff = 2000):
 #     rider_sub_queries = []
--- pc_predictor.py
+++ pc_predictor.py
@@ -1,13 +1,92 @@
 import pc_db as db
 from pc_race import load
+from dateutil import parser
+import numpy as np
 
 import matplotlib.pyplot as plt
 
-race = load("race.json")
-riders = race['riders']
+stage_points = [
+    50,
+    44,
+    40,
+    36,
+    32,
+    30,
+    28,
+    26,
+    24,
+    22,
+    20,
+    18,
+    16,
+    14,
+    12,
+    10,
+    8,
+    6,
+    4,
+    2
+]
 
-for rider in riders:
-    plt.plot(*db.date_result_query(rider, 'REGULAR', 10, 2018), label = rider)
+def std_difference(mean, std, x):
+    return (x - mean) / std
 
+def get_rider_itt_score_graph(rider, date_cutoff = '2030-12-31'):
+    races = db.rider_race_query(rider, 'ITT', 999, date_cutoff)
+    dates = []
+    scores = []
+    for race, speed, date in races:
+        mean, std = db.get_statistics_for_race(race)
+        dates.append(parser.parse(date))
+        scores.append(std_difference(mean, std, speed))
+    return (dates, scores)
+
+def get_rider_itt_score(rider, date_cutoff = '2030-12-31'):
+    _, scores = get_rider_itt_score_graph(rider, date_cutoff)
+    if len(scores) == 0:
+        return 0
+    weights = np.linspace(1, len(scores) + 1, len(scores))
+    return np.average(scores, None, weights)
+
+def sort_dict_by_values(dicty):
+    return {k: v for k, v in sorted(dicty.items(), key=lambda item: item[1])}
+
+def predict_race(slug):
+    riders = db.get_riders_of_race(slug)
+    date = db.get_date_of_race(slug)
+    rider_scores = {}
+    for rider in riders:
+        rider_scores[rider] = get_rider_itt_score(rider, date)
+    best_riders = list(reversed(list(sort_dict_by_values(rider_scores).keys())))[:20]
+    total_points = 0
+    for rider in best_riders:
+        if rider in riders[:20]:
+            total_points += stage_points[riders.index(rider)]
+    print(best_riders)
+    print("Total points: %d" % total_points)
+
+# predict_race('vuelta-a-espana/2019/stage-10')
+
+# get_rider_itt_score_graph('tom-dumoulin')
+# mean, std = db.get_statistics_for_race("tour-de-france/2019/stage-13")
+# mean, std = db.get_statistics_for_race("tour-de-france/2019/stage-13")
+# print(mean)
+# print(std)
+# for rider in riders:
+#     plt.plot(*db.date_result_query(rider, 'REGULAR', 10, 2019), label = rider)
+
+# for rider in riders:
+#     dates, scores = get_rider_itt_score_graph(rider)
+#     plt.plot(dates, scores, label = rider)
+#     weights = np.arange(1, len(scores) + 1, 1)
+#     print(weights)
+#     print(len(weights))
+#     print(len(scores))
+#     print("%s average score: %f" % (rider, )
+
+
+
+dates, scores = get_rider_itt_score_graph('nathan-van-hooydonck')
+plt.plot(dates, scores)
 plt.legend(loc="upper left")
 plt.show()
